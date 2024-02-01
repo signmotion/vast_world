fbee0356ebc247964fd2112a25458f182139c182
Andrii Syrokomskyi <signmotion@gmail.com>
2024-02-01 20:03:52 +0100

    Overloaded Quant.isCorrectUid.

--- lib/src/share/quant/quant.dart
+++ lib/src/share/quant/quant.dart
@@ -8,4 +8,10 @@ abstract class Quant with HasProtoBaseMix, HasStringIdMix {
     this.hid = hid;
     this.uid = uid;
   }
+
+  /// Should be implemented for each descendant.
+  @override
+  bool get isCorrectUid =>
+      throw UnimplementedError('Should be defined for each descendant and'
+          ' consider to [is*Uid] checkers.');
 }
