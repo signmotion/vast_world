7d04fe59f8f33c1cd8b22c735708d1937cc1ab87
Andrii Syrokomskyi <signmotion@gmail.com>
2024-02-01 20:05:59 +0100

    Added checkers for specific HIDs.

--- lib/src/share/utils/uid.dart
+++ lib/src/share/utils/uid.dart
@@ -15,10 +15,13 @@ String get genSessionUid => '$sessionUidPrefix-$genUuid';
 /// \see [genUuid]
 extension UidExt on String {
   bool get isActUid => isUuidWithPrefix(actUidPrefix);
+  bool get isActHid => isNotEmpty && !isActUid;
 
   bool get isComponentUid => isUuidWithPrefix(componentUidPrefix);
+  bool get isComponentHid => isNotEmpty && !isComponentUid;
 
   bool get isPlanUid => isUuidWithPrefix(planUidPrefix);
+  bool get isPlanHid => isNotEmpty && !isPlanUid;
 
   bool get isSessionUid => isUuidWithPrefix(sessionUidPrefix);
 }
