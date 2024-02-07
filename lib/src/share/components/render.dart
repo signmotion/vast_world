c009dba97a71c01474799b4b1904807017b1f1b6
Andrii Syrokomskyi <signmotion@gmail.com>
2024-02-07 18:13:02 +0100

    Made RenderComponent.value as unimplemented. Reason: We should declare own renders with initialized render functions for avoid hidden errors. See the project `fairy_journey`, folder `render`.

--- lib/src/share/components/render.dart
+++ lib/src/share/components/render.dart
@@ -5,6 +5,10 @@ part of '../../../vast_world_share.dart';
 /// Constructed by analogy [PlanListComponent].
 /// Use [unimplementedRender] to stub the Client only render on the Server side.
 abstract class RenderComponent<T> extends Component<RenderFn<T>> {
+  @override
+  set value(RenderFn<T>? v) =>
+      throw UnsupportedError('Just override [value] for return function.');
+
   RenderFn<T> get render => value;
 
   RenderedData<T> renderData(Plan<dynamic> spectator, Plan<dynamic> watched) =>
