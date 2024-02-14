3789e84d1bbff9aab9c23244cb1c05a589b4517d
Andrii Syrokomskyi <signmotion@gmail.com>
2024-02-14 17:26:45 +0100

    Removed an unused protofile Content.

--- lib/runes/fantasy_country_conceiver.dart
+++ lib/runes/fantasy_country_conceiver.dart
@@ -4,7 +4,6 @@ import 'package:base_server/base_server.dart';
 import 'package:grpc/grpc.dart' as grpc;
 import 'package:thematic_names/thematic_names.dart';
 
-import '../src/share/generated/protos/vw/runes/fantasy_country_conceiver/content.pb.dart';
 import '../src/share/generated/protos/vw/runes/fantasy_country_conceiver/service.pbgrpc.dart';
 
 import '../vast_world_maia.dart' as maia hide ServiceBase;
@@ -13,14 +12,9 @@ import '../vast_world_share.dart' hide ServiceBase;
 export 'package:grpc/grpc.dart' hide Client, Server, Service;
 export 'package:protobuf/protobuf.dart';
 
-export '../src/share/generated/protos/vw/runes/fantasy_country_conceiver/content.pb.dart';
-export '../src/share/generated/protos/vw/runes/fantasy_country_conceiver/content.pbenum.dart';
-export '../src/share/generated/protos/vw/runes/fantasy_country_conceiver/content.pbjson.dart';
-
 export '../src/share/generated/protos/vw/runes/fantasy_country_conceiver/service.pb.dart';
 export '../src/share/generated/protos/vw/runes/fantasy_country_conceiver/service.pbenum.dart';
 export '../src/share/generated/protos/vw/runes/fantasy_country_conceiver/service.pbgrpc.dart';
 export '../src/share/generated/protos/vw/runes/fantasy_country_conceiver/service.pbjson.dart';
 
-part '../src/runes/fantasy_country_conceiver/content.dart';
 part '../src/runes/fantasy_country_conceiver/service.dart';
--- lib/src/runes/fantasy_country_conceiver/content.dart
+++
@@ -1,4 +0,0 @@
-part of '../../../runes/fantasy_country_conceiver.dart';
-
-/// The content of this rune.
-typedef Content = ContentBase;
